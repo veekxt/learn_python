11f9c0de5b3c0e5f0f628aeeb370b5b157409009
XieTao <veekxt@gmail.com>
2015-11-06 18:03:52 +0800

    update re

--- xt_re.py
+++ xt_re.py
@@ -10,8 +10,12 @@ print(s.group(1))
 g=s.group(0)
 print(g)
 
-#等价
+#等价，search搜索第一个
 s=re.search(r'(te)st(?= )','it is a test text')
 print(s.group(1))
 g=s.group(0)
 print(g)
+
+#findall  返回所有匹配字符串
+s=re.findall(r'is','it is a test text is one ')
+print(s)
