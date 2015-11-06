32b0f195eedbc1c5d81e54966154be537ad758a9
XieTao <veekxt@gmail.com>
2015-11-06 20:51:41 +0800

    add urllib

--- xt_urllib.py
+++ xt_urllib.py
@@ -8,7 +8,7 @@ f=open('tmp.txt','w')
 f.write(r.read().decode('utf-8'))
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'}
-r=urllib.request.Request(url='http://www.useragentstring.com/',headers=headers)
+r=urllib.request.Request(url='http://www.useragentstring.com',headers=headers)
 s=urllib.request.urlopen(r)
 f=open('tmp2.html','w')
 f.write(s.read().decode('utf-8'))
