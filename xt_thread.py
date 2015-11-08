e27cbd701bd475a8e33686a84655cef78d811092
XieTao <veekxt@gmail.com>
2015-11-08 23:45:19 +0800

    upadte thread

--- xt_thread.py
+++ xt_thread.py
@@ -2,5 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import time  
-import thread
+import threading
 
+def print_str(tmp,delay_time):
+    while 1:
+        print(tmp)
+        time.sleep(delay_time)
+        
+A=threading.Thread(target=print_str,args=('AAAA',1))
+B=threading.Thread(target=print_str,args=('BBBB',2))
+A.start()
+B.start()
