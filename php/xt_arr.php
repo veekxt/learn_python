ae3bf3f6ebf708d42d3dae3f11d72f4ac1a2233b
XieTao <veekxt@gmail.com>
2015-11-09 21:34:16 +0800

    update php

--- php/xt_arr.php
+++ php/xt_arr.php
@@ -2,3 +2,16 @@
 $myarr = array('foo'=>'bar','bar'=>'foo');
 $myarr2 = ['foo'=>'bar2','bar'=>'foo2','2'=>'1','last'];
 var_dump($myarr2);
+
+var_dump($myarr2[3]);
+
+$array = array(
+    "foo" => "bar",
+    42    => 24,
+    "multi" => array(
+         "dimensional" => array(
+             "array" => "foo"
+         )
+    )
+);
+var_dump($array["multi"]["dimensional"]["array"]);
