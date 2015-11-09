8f5f59aa7eb77266a1f7354865262b9f85991953
XieTao <veekxt@gmail.com>
2015-11-09 21:43:36 +0800

    update php

--- php/xt_arr.php
+++ php/xt_arr.php
@@ -4,9 +4,11 @@ $myarr2 = ['foo'=>'bar2','bar'=>'foo2','2'=>'1','last'];
 var_dump($myarr2);
 
 var_dump($myarr2[3]);
-
-$myarr[] = 'who'
-
+//增加
+$myarr[] = 'who';
+$myarr['add'] = 'mya';
+//删除
+unset($myarr['add']);
 var_dump($myarr);
 
 $array = array(
