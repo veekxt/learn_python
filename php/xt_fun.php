0890e526d82297cabedd59943d95b043a529ddb9
XieTao <veekxt@gmail.com>
2015-11-10 23:15:07 +0800

    update php_xt_fun

--- php/xt_fun.php
+++ php/xt_fun.php
@@ -5,4 +5,15 @@ function xt_sum($a,$b)
     return $a+$b;
 }
 
-echo xt_sum(4,5)
+echo xt_sum(4,5),"\n";
+
+//可变参数
+function sum(...$numbers) {
+    $acc = 0;
+    foreach ($numbers as $n) {
+        $acc += $n;
+    }
+    return $acc;
+}
+
+echo sum(1, 2, 3, 4)."\n";
