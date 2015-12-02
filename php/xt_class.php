0dd4db47e4fd26a80b89ef41374a73f5df8ec474
XieTao <veekxt@gmail.com>
2015-12-02 21:06:34 +0800

    add php class test

--- php/xt_class.php
+++ php/xt_class.php
@@ -2,16 +2,21 @@
 //基本
 class SimpleClass
 {
+    const constant = 'constant value';
+    function showConstant() {
+        echo  self::constant . "\n";
+    }
     // property declaration
     public $var = 'a default value';
 
     // method declaration
     public function displayVar() {
-        echo $this->var;
+        echo $this->var."\n";
     }
 }
 $a=new SimPleClass();
 $a->displayVar();
+$a->showConstant();
 
 echo SimpleClass::class;
 ?>
