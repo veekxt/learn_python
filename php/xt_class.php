0300f1efba1f781f4522851b27b378b0ba84600a
XieTao <veekxt@gmail.com>
2015-12-02 20:59:25 +0800

    add php class test

---
+++ php/xt_class.php
@@ -0,0 +1,18 @@
+<?php
+//基本
+class SimpleClass
+{
+    // property declaration
+    public $var = 'a default value';
+
+    // method declaration
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+$a=new SimPleClass();
+$a->displayVar();
+
+
+?>
+
