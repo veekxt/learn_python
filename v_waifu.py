8e208db9bb7afdf76d22f7fce4f719311144e01b
veekxt <veekxt@gmail.com>
2019-02-20 22:36:00 +0800

    v_waifu bug fix

--- v_waifu.py
+++ v_waifu.py
@@ -7,15 +7,15 @@ opener.addheaders = [('User-agent',
                       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-def get_waifu(n,err,i):
+def get_waifu(n,next_do,i):
     try:
-        request = urllib.request.urlopen('http://www.thiswaifudoesnotexist.net/example-' + str(i) + '.jpg', timeout=5)
-        with open('D:/wf/' + 'example-' + str(i) + '.jpg', 'wb') as f:
+        request = urllib.request.urlopen('http://www.thiswaifudoesnotexist.net/example-' + str(n) + '.jpg', timeout=5)
+        with open('D:/wf/' + 'example-' + str(n) + '.jpg', 'wb') as f:
             f.write(request.read())
-            err.pop(i)
-            print('down-ok-' + str(i))
+            print('down-ok-' + str(n))
     except:
-        print("error" + str(i))
+        print("error" + str(n))
+        next_do.append(n)
 
 # for i in range(1, 40001):
 #     local_filename, headers = urllib.request.urlretrieve(
@@ -25,13 +25,16 @@ def get_waifu(n,err,i):
 #     f.close()
 #     print('down-ok-' + str(i))
 #     time.sleep(5)
-err = list(range(1,40001))
+err = list(range(1,20))
 
 while True:
+    next_do = []
     for i,v in enumerate(err):
-        get_waifu(v,err,i)
-    if len(err) == 0:
+        print("v is "+str(v))
+        get_waifu(v,next_do,i)
+    if len(next_do) == 0:
         break
     else:
+        err = next_do
         continue
 
