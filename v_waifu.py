c1926078b1b9222fbd99ccf583d5654b7dec3ffd
veekxt <veekxt@gmail.com>
2019-02-21 02:52:26 +0800

    v_waifu bmulti-thread

--- v_waifu.py
+++ v_waifu.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import urllib
 import urllib.request
@@ -7,33 +8,41 @@ opener.addheaders = [('User-agent',
                       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-def get_waifu(n,next_do,i):
+
+def get_waifu(n, next_do, i):
     try:
         request = urllib.request.urlopen('http://www.thiswaifudoesnotexist.net/example-' + str(n) + '.jpg', timeout=5)
-        with open('D:/wf/' + 'example-' + str(n) + '.jpg', 'wb') as f:
+        with open('example-' + str(n) + '.jpg', 'wb') as f:
             f.write(request.read())
             print('down-ok-' + str(n))
     except:
-        print("error" + str(n))
+        print("[retry later] error-" + str(n))
         next_do.append(n)
 
-# for i in range(1, 40001):
-#     local_filename, headers = urllib.request.urlretrieve(
-#         'http://www.thiswaifudoesnotexist.net/example-' + str(i) + '.jpg',
-#         'wf/' + 'example-' + str(i) + '.jpg')
-#     f = open(local_filename)
-#     f.close()
-#     print('down-ok-' + str(i))
-#     time.sleep(5)
-err = list(range(1,40001))
 
-while True:
-    next_do = []
-    for i,v in enumerate(err):
-        get_waifu(v,next_do,i)
-    if len(next_do) == 0:
-        break
-    else:
-        err = next_do
-        continue
+def down(start, stop):
+    err = list(range(start, stop))
+    while True:
+        next_do = []
+        for i, v in enumerate(err):
+            get_waifu(v, next_do, i)
+        if len(next_do) == 0:
+            break
+        else:
+            err = next_do
+            continue
 
+
+def sp(start, stop, num):
+    start_s = start
+    step = (stop - start) // num
+    while start_s != stop:
+        stop_s = start_s + step
+        if stop_s >= stop:
+            stop_s = stop
+        tmp = threading.Thread(target=down, args=(start_s, stop_s))
+        tmp.start()
+        start_s = stop_s
+
+
+sp(1, 40001, 64)
